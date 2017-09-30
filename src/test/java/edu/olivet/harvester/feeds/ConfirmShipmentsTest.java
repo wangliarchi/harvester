b60e34364f2f34c0155d6fe20dc959707841ea48
Johnny <alentoy@gmail.com>
2017-09-30 09:56:59 -0400

    1. save order confirmation log to local sqlite db, add view history function 2. add migration configuration from OrderMan 3. add some more unit tests

--- src/test/java/edu/olivet/harvester/feeds/ConfirmShipmentsTest.java
+++ src/test/java/edu/olivet/harvester/feeds/ConfirmShipmentsTest.java
@@ -4,10 +4,14 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONException;
 import com.google.inject.Inject;
+import edu.olivet.foundations.amazon.Country;
+import edu.olivet.foundations.amazon.FeedUploader;
+import edu.olivet.foundations.amazon.MarketWebServiceIdentity;
 import edu.olivet.foundations.utils.RegexUtils;
 import edu.olivet.foundations.utils.Strings;
 import edu.olivet.foundations.utils.Tools;
 import edu.olivet.harvester.common.BaseTest;
+import edu.olivet.harvester.feeds.helper.FeedGenerator;
 import edu.olivet.harvester.model.Order;
 import edu.olivet.harvester.model.OrderEnums;
 import edu.olivet.harvester.spreadsheet.Spreadsheet;
@@ -16,6 +20,7 @@ import edu.olivet.harvester.spreadsheet.exceptions.NoOrdersFoundInWorksheetExcep
 import edu.olivet.harvester.spreadsheet.exceptions.NoWorksheetFoundException;
 import edu.olivet.harvester.spreadsheet.service.AppScript;
 import edu.olivet.harvester.spreadsheet.service.OrderHelper;
+import edu.olivet.harvester.utils.Settings;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
@@ -121,6 +126,32 @@ public class ConfirmShipmentsTest extends  BaseTest {
 
         date = formatter.parse("10/08/2017").getTime();
         assertEquals(confirmShipments.getSheetNameByDate(date),"10/08");
+    }
+
+    @Inject private FeedUploader feedUploader;
+
+    @Test
+    public void testSubmitFeed() {
+
+        Settings.Configuration config = Settings.load(basePath+File.separator+"conf/harvester-test-invalidmws.json").getConfigByCountry(Country.US);
+
+        MarketWebServiceIdentity credential = config.getMwsCredential();
+
+        File feedFile = new File(TEST_DATA_ROOT + File.separator + "feed-US_BOO_confirm_shipment-invalid.txt");
+        String result = feedUploader.execute(feedFile, FeedGenerator.BatchFileType.ShippingConfirmation.feedType(), credential, 1);
+
+    }
+
+    @Test
+    public  void  testInsertToLocalDbLog() {
+
+        String submissionResult = "Feed Processing Summary:\n" +
+                "\tNumber of records processed\t\t7\n" +
+                "\tNumber of records successful\t\t7";
+
+        File feedFile = new File(TEST_DATA_ROOT+File.separator+"feed-US_BOOK_confirm_shipment_2017-9-28_120813.txt");
+
+        confirmShipments.insertToLocalDbLog(feedFile, Country.US, submissionResult);
     }
     @Test
     public void testGenerateFeedFile() throws Exception {
