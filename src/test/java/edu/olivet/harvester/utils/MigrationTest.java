15d4b9d8acdd4c1c22edac8efaeecd61ef35b055
Johnny <alentoy@gmail.com>
2017-10-03 10:13:17 -0400

    fixed configuration migration bug checkstyle fixes code refactoring

--- src/test/java/edu/olivet/harvester/utils/MigrationTest.java
+++ src/test/java/edu/olivet/harvester/utils/MigrationTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 /**
  * @author <a href="mailto:rnd@olivetuniversity.edu">RnD</a> 9/30/17 9:48 AM
@@ -19,9 +19,29 @@ public class MigrationTest extends BaseTest {
 
         assertEquals(settings.getSid(), "714");
         assertEquals(settings.getConfigByCountry(Country.US).getStoreName(), "Mike Pro");
+
+        assertEquals(settings.getConfigByCountry(Country.US).getMwsCredential().getSellerId(),"A3BEPQLI451F6I");
         assertEquals(settings.getConfigByCountry(Country.US).getMwsCredential().getAccessKey(), "AKIAI73MGXM4FSWB6TIQ");
+        assertEquals(settings.getConfigByCountry(Country.US).getMwsCredential().getSecretKey(), "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");
+
+        assertEquals(settings.getConfigByCountry(Country.US).getSeller().getEmail(),"mike.barrpro@gmail.com");
+        assertEquals(settings.getConfigByCountry(Country.US).getSeller().getPassword(),"BarrP78Fly");
+
+        assertEquals(settings.getConfigByCountry(Country.US).getBuyer().getEmail(),"buyerus@gmail.com");
+        assertEquals(settings.getConfigByCountry(Country.US).getBuyer().getPassword(),"buyeruspw");
+
+        assertEquals(settings.getConfigByCountry(Country.US).getProdBuyer(), null);
+
+
+        assertEquals(settings.getConfigByCountry(Country.US).getPrimeBuyer().getEmail(),"pbuyerus@gmail.com");
+        assertEquals(settings.getConfigByCountry(Country.US).getPrimeBuyer().getPassword(),"pbuyeruspw");
 
         assertEquals(settings.getConfigByCountry(Country.US).getBookDataSourceUrl(), "1IMbmaLUjqvZ7w8OdPd59fpTuad8U__5PAyKg3yR0DjY");
+        assertEquals(settings.getConfigByCountry(Country.US).getProductDataSourceUrl(), "");
+
+
+        assertEquals(settings.getConfigByCountry(Country.MX).getBuyer(),null);
+        assertEquals(settings.getConfigByCountry(Country.MX).getProdBuyer().getEmail(),"mxprobuyer@gmail.com");
     }
 
 }
