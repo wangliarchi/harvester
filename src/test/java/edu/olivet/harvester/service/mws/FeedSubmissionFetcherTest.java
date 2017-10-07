d35390770c185aa7ea5bad61ad2902a6bb7c8276
Johnny <alentoy@gmail.com>
2017-10-07 10:05:52 -0400

    made submission list result more readable.

--- src/test/java/edu/olivet/harvester/service/mws/FeedSubmissionFetcherTest.java
+++ src/test/java/edu/olivet/harvester/service/mws/FeedSubmissionFetcherTest.java
@@ -13,8 +13,6 @@ import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author <a href="mailto:rnd@olivetuniversity.edu">OU RnD</a> 10/7/17 2:20 AM
@@ -34,12 +32,17 @@ public class FeedSubmissionFetcherTest {
         MarketWebServiceIdentity credential = Settings.load().getConfigByCountry(Country.US).getMwsCredential();
         //TypeList feedTypeList = new TypeList(Collections.singletonList("_POST_FLAT_FILE_FULFILLMENT_DATA_"));
         TypeList feedTypeList = new TypeList();
-        StatusList feedProcessingStatusList = new StatusList(Stream.of("_IN_PROGRESS_", "_SUBMITTED_").collect(Collectors.toList()));
+        StatusList feedProcessingStatusList = new StatusList();
 
         List<FeedSubmissionInfo> result
                 = feedSubmissionFetcher.getFeedSubmissionList(credential, feedTypeList, feedProcessingStatusList);
 
-        System.out.println(result.toString());
+        StringBuilder submissions = new StringBuilder();
+        result.forEach(it -> {
+            submissions.append(String.format("FeedSubmissionId %s submitted at %s, current status %s \n", it.getFeedSubmissionId(),it.getSubmittedDate(),it.getFeedProcessingStatus()));
+        });
+
+        System.out.println(submissions.toString());
 
     }
 
