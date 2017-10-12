c6241fb2b684899ca233f85c9dbefd503c7ef2d6
Johnny PC <jxiang@olivetuniversity.edu>
2017-10-12 10:55:25 -0700

    check if harvester is running. if it is, exist

--- script/StartApplication.bat
+++ script/StartApplication.bat
@@ -1,15 +1,20 @@
 @echo off
-set SLEEP_TME=5
 if not exist C:\Harvester\Harvester.exe (
-	echo It seems that Harvester was not installed properly. Please fix this error first.
-	exit
+  echo It seems that Harvester was not installed properly. Please fix this error first.
+  exit
 )
 cd /d C:\Harvester
-if exist "app-data\customize\harvester-config.json" (
-	start Harvester.exe
-	echo Harvester was started successfully.
-	echo This dialog box will be closed after %SLEEP_TME% seconds.
-) else (
-	echo Harvester configuration file missing, it is meaningless to start it.
+if not exist "app-data\customize\harvester-config.json" (
+  echo Harvester configuration file missing, it is meaningless to start it.
+  exit
 )
-timeout %SLEEP_TME%
+
+wmic process list brief | find /i "Harvester.exe"
+if %ERRORLEVEL% == 0 (
+  echo Harvester is running. There is no need to start it again.
+) else (
+  echo No running process of Harvester was found and we will start it directly.
+  cd /d C:\Harvester
+  start Harvester.exe
+  echo Harvester was started successfully.
+)
