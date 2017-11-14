ace20f47ae742518a7a2711c6bc9fe6651ce0ea2
Johnny PC <jxiang@olivetuniversity.edu>
2017-11-14 13:49:37 -0800

    refactoring

--- src/main/java/edu/olivet/harvester/fulfill/model/OrderFulfillmentRecord.java
+++ src/main/java/edu/olivet/harvester/fulfill/model/OrderFulfillmentRecord.java
@@ -17,7 +17,7 @@ import java.util.Date;
 
 @Table(value = "order_fulfillment_record")
 @Data
-@EqualsAndHashCode()
+@EqualsAndHashCode(callSuper = false)
 public class OrderFulfillmentRecord extends PrimaryKey implements ArrayConvertable {
     @Name
     private String id;
@@ -75,28 +75,3 @@ public class OrderFulfillmentRecord extends PrimaryKey implements ArrayConvertab
 
     //public static final int[] WIDTHS = {250, 150};
 }
-
-/**
- * CREATE TABLE IF NOT EXISTS order_fulfillment_record (
- * id         VARCHAR(40) PRIMARY KEY NOT NULL,
- * orderId    VARCHAR(25)             NOT NULL DEFAULT '',
- * sku    VARCHAR(100)             NOT NULL DEFAULT '',
- * sheetName    VARCHAR(25)             NOT NULL DEFAULT '',
- * spreadsheetId VARCHAR(100)             NOT NULL DEFAULT '',
- * purchaseDate    VARCHAR(25)             NOT NULL DEFAULT '',
- * isbn   VARCHAR(10)             NOT NULL DEFAULT '',
- * seller   VARCHAR(100)             NOT NULL DEFAULT '',
- * sellerId   VARCHAR(30)             NOT NULL DEFAULT '',
- * sellerPrice   VARCHAR(10)             NOT NULL DEFAULT '',
- * condition   VARCHAR(20)             NOT NULL DEFAULT '',
- * character   VARCHAR(10)             NOT NULL DEFAULT '',
- * remark   VARCHAR(100)             NOT NULL DEFAULT '',
- * quantityPurchased INTEGER(3) NOT NULL DEFAULT 0,
- * quantityBought INTEGER(3) NOT NULL DEFAULT 0,
- * cost   VARCHAR(10)             NOT NULL DEFAULT '',
- * orderNumber    VARCHAR(25)             NOT NULL DEFAULT '',
- * buyerAccount    VARCHAR(100)             NOT NULL DEFAULT '',
- * lastCode    VARCHAR(10)             NOT NULL DEFAULT '',
- * fulfillDate    DATETIME                NOT NULL
- * );
- */
