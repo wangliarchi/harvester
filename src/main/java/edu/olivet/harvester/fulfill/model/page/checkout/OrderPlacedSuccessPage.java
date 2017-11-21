c2b1ad5f2fc4024096586886811793b3b396b437
Johnny <alentoy@gmail.com>
2017-11-21 14:55:31 -0500

    bug fixes

--- src/main/java/edu/olivet/harvester/fulfill/model/page/checkout/OrderPlacedSuccessPage.java
+++ src/main/java/edu/olivet/harvester/fulfill/model/page/checkout/OrderPlacedSuccessPage.java
@@ -1,17 +1,21 @@
 package edu.olivet.harvester.fulfill.model.page.checkout;
 
 import com.teamdev.jxbrowser.chromium.dom.DOMElement;
+import edu.olivet.foundations.amazon.Country;
 import edu.olivet.foundations.utils.RegexUtils;
 import edu.olivet.harvester.fulfill.model.page.FulfillmentPage;
 import edu.olivet.harvester.model.Order;
 import edu.olivet.harvester.ui.BuyerPanel;
 import edu.olivet.harvester.utils.JXBrowserHelper;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author <a href="mailto:rnd@olivetuniversity.edu">OU RnD</a> 11/9/17 4:35 PM
  */
 public class OrderPlacedSuccessPage extends FulfillmentPage {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderPlacedSuccessPage.class);
 
     public OrderPlacedSuccessPage(BuyerPanel buyerPanel) {
         super(buyerPanel);
@@ -22,32 +26,32 @@ public class OrderPlacedSuccessPage extends FulfillmentPage {
 
         JXBrowserHelper.waitUntilVisible(browser, ".a-alert-content a.a-link-emphasis");
         JXBrowserHelper.saveOrderScreenshot(order, buyerPanel, "1");
-
+        DOMElement viewLink = JXBrowserHelper.selectElementByCssSelectorWaitUtilLoaded(browser, ".a-alert-content a.a-link-emphasis");
         //parse orderId from url
         String orderId = RegexUtils.getMatched(browser.getURL(), RegexUtils.Regex.AMAZON_ORDER_NUMBER);
-        if (StringUtils.isNotBlank(orderId)) {
-            order.order_number = orderId;
-            JXBrowserHelper.loadPage(browser, String.format("%s/gp/css/summary/edit.html/ref=typ_rev_edit?ie=UTF8&orderID=%s", buyerPanel.getCountry().baseUrl(), orderId));
-            return;
+        if (StringUtils.isBlank(orderId)) {
+            //view order link
+            orderId = RegexUtils.getMatched(viewLink.getAttribute("href"), RegexUtils.Regex.AMAZON_ORDER_NUMBER);
         }
 
 
-        //view order link
-        DOMElement viewLink = JXBrowserHelper.selectElementByCssSelectorWaitUtilLoaded(browser, ".a-alert-content a.a-link-emphasis");
-        orderId = RegexUtils.getMatched(viewLink.getAttribute("href"), RegexUtils.Regex.AMAZON_ORDER_NUMBER);
         if (StringUtils.isNotBlank(orderId)) {
             order.order_number = orderId;
-            JXBrowserHelper.loadPage(browser, String.format("%s/gp/css/summary/edit.html/ref=typ_rev_edit?ie=UTF8&orderID=%s", buyerPanel.getCountry().baseUrl(), orderId));
-            return;
+            String url = String.format("%s/gp/css/summary/edit.html/ref=typ_rev_edit?ie=UTF8&orderID=%s", buyerPanel.getCountry().baseUrl(), orderId);
+            LOGGER.debug("Order placed succussfully, with order id {}. Now heading to {}", orderId, url);
+            JXBrowserHelper.loadPage(browser, url);
+        } else {
+            viewLink.click();
+            JXBrowserHelper.waitUntilNewPageLoaded(browser);
         }
 
-        viewLink.click();
-        JXBrowserHelper.waitUntilNewPageLoaded(browser);
+
     }
 
     public static void main(String[] args) {
         String url = "https://www.amazon.com/gp/buy/thankyou/handlers/display.html?ie=UTF8&asins=B005LTNLDS&isRefresh=1&orderId=113-5188884-1320230&purchaseId=106-9921921-2849009&viewId=ThankYouCart";
         String orderId = RegexUtils.getMatched(url, RegexUtils.Regex.AMAZON_ORDER_NUMBER);
-        System.out.println(orderId);
+        String vUrl = String.format("%s/gp/css/summary/edit.html/ref=typ_rev_edit?ie=UTF8&orderID=%s", Country.US.baseUrl(), orderId);
+        System.out.println(vUrl);
     }
 }
