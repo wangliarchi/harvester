dd5e74b39110e2109f9bbca2e4cd471a627da24d
Johnny PC <jxiang@olivetuniversity.edu>
2018-02-15 14:02:34 -0500

    reload order data to check before placing order

--- src/main/java/edu/olivet/harvester/fulfill/service/steps/PlaceOrder.java
+++ src/main/java/edu/olivet/harvester/fulfill/service/steps/PlaceOrder.java
@@ -1,12 +1,16 @@
 package edu.olivet.harvester.fulfill.service.steps;
 
 import com.google.inject.Inject;
+import edu.olivet.harvester.common.model.Order;
+import edu.olivet.harvester.fulfill.exception.Exceptions.OrderSubmissionException;
 import edu.olivet.harvester.fulfill.model.page.checkout.CheckoutEnum;
 import edu.olivet.harvester.fulfill.model.page.checkout.OrderReviewMultiPage;
 import edu.olivet.harvester.fulfill.model.page.checkout.OrderReviewOnePage;
+import edu.olivet.harvester.fulfill.service.SheetService;
 import edu.olivet.harvester.fulfill.service.StepHelper;
 import edu.olivet.harvester.fulfill.service.flowcontrol.FlowState;
 import edu.olivet.harvester.fulfill.service.flowcontrol.Step;
+import edu.olivet.harvester.fulfill.utils.OrderStatusUtils;
 import edu.olivet.harvester.utils.MessageListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +21,15 @@ import org.slf4j.LoggerFactory;
 public class PlaceOrder extends Step {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlaceOrder.class);
 
-    @Inject
-    MessageListener messageListener;
+    @Inject SheetService sheetService;
 
     //dispatcher method
     protected void process(FlowState state) {
+        //
+        Order order = sheetService.reloadOrder(state.getOrder());
+        if (!order.fulfillable()) {
+            throw new OrderSubmissionException("Order status [" + order.status + "] is not marked for fulfillment.");
+        }
 
         if (stepHelper.detectCurrentPage(state) == CheckoutEnum.CheckoutPage.ShippingMethodOnePage) {
             OrderReviewOnePage orderReviewOnePage = new OrderReviewOnePage(state.getBuyerPanel());
