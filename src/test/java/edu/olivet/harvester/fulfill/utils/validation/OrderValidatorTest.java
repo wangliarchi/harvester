97c15c96b2f6b16a4b64e318be0ad224b471c02a
Johnny PC <jxiang@olivetuniversity.edu>
2018-03-23 10:50:57 -0400

    shipping address changed unit test

--- src/test/java/edu/olivet/harvester/fulfill/utils/validation/OrderValidatorTest.java
+++ src/test/java/edu/olivet/harvester/fulfill/utils/validation/OrderValidatorTest.java
@@ -1,11 +1,26 @@
 package edu.olivet.harvester.fulfill.utils.validation;
 
+import com.google.inject.Inject;
 import edu.olivet.harvester.common.BaseTest;
+import edu.olivet.harvester.common.model.Order;
+import edu.olivet.harvester.fulfill.utils.validation.OrderValidator.Validator;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.*;
+
 public class OrderValidatorTest extends BaseTest {
+    @Inject OrderValidator orderValidator;
+
     @Test
     public void notSelfOrder() {
+        Order order = prepareOrder();
+        order.remark = "change address";
+
+        String result = orderValidator.validWithValidators(order, Validator.AddressNotChanged);
+        System.out.println(result);
+
+        assertTrue(StringUtils.isNotBlank(result));
 
     }
 
