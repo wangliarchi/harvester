e2531a1cb0a978931be56591de47e4bf480879f3
Johnny PC <jxiang@olivetuniversity.edu>
2018-04-13 16:26:33 -0400

    refactoring

--- src/main/java/edu/olivet/harvester/letters/model/GrayEnums.java
+++ src/main/java/edu/olivet/harvester/letters/model/GrayEnums.java
@@ -5,12 +5,40 @@ package edu.olivet.harvester.letters.model;
  */
 public class GrayEnums {
     public enum GrayLetterType {
-        SG,
-        NG,
-        WC,
-        HP,
-        PH,
-        LW,
-        DN
+        SG(false, true),
+        NG(true, true),
+        WC(true, false),
+        HP(true, true),
+        PH(true, true),
+        LW(true, true),
+        DN(true, true);
+
+        private boolean shouldBeShipped;
+        private boolean sendImmediately;
+
+        GrayLetterType(boolean sendImmediately, boolean shouldBeShipped) {
+            this.sendImmediately = sendImmediately;
+            this.shouldBeShipped = shouldBeShipped;
+        }
+
+        public static GrayLetterType getTypeFromStatus(String status) {
+            for (GrayLetterType type : GrayLetterType.values()) {
+                if (type.name().equalsIgnoreCase(status.trim())) {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+
+        public boolean handleImmediately() {
+            return sendImmediately;
+        }
+
+        public boolean isShouldBeShipped() {
+            return shouldBeShipped;
+        }
+
     }
+    //"ng", "hp", "ph", "wc", "lw", "dn"
 }
--- src/main/java/edu/olivet/harvester/letters/service/GrayLetterRule.java
+++ src/main/java/edu/olivet/harvester/letters/service/GrayLetterRule.java
@@ -3,8 +3,11 @@ package edu.olivet.harvester.letters.service;
 import edu.olivet.foundations.utils.Dates;
 import edu.olivet.foundations.utils.Strings;
 import edu.olivet.harvester.common.model.Order;
+import edu.olivet.harvester.letters.model.GrayEnums.GrayLetterType;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
@@ -13,6 +16,7 @@ import java.util.Date;
  */
 public class GrayLetterRule {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrayLetterRule.class);
 
     public enum GrayRule {
         SendLetter,
@@ -25,20 +29,18 @@ public class GrayLetterRule {
             "大", "超大超重", "不健康", "易碎", "危险品", "黑名单", "电器", "big", "heavy"
     };
 
-    private static final String[] GRAY_LETTER_REMARKS = {"ng", "hp", "ph", "wc", "lw", "dn"};
-    private static final String[] COMMON_GRAY_LETTER_REMARKS = {"sg"};
-
     //a.对于7天内的灰条(status为sg，remark含有sg/high price/price/价格)重新找单,找单成功后变成可做单（白条），对于第7-10天的灰条发灰条信
     //b.检查两天内的status（sg）和remark，对于remark含有 检查已经confirm后 随即发灰条信
     //c.检查两天内的status和remark，对于status和remark标有ng，wc，hp，ph，lw，检查已经confirm后随即发灰条信（wc除外）
     //d.对于us的快递单灰条（运费为6.99），重新找单失败后，检查已经confirm后随即发灰条信
     public static GrayRule getGrayRule(Order order, boolean huntSupplier) {
 
-        if (!StringUtils.equalsAnyIgnoreCase(order.status, ArrayUtils.addAll(GRAY_LETTER_REMARKS, COMMON_GRAY_LETTER_REMARKS))) {
+        if (order.buyerCanceled()) {
             return GrayRule.None;
         }
 
-        if (order.buyerCanceled()) {
+        GrayLetterType type = GrayLetterType.getTypeFromStatus(order.status);
+        if (type == null) {
             return GrayRule.None;
         }
 
@@ -55,7 +57,12 @@ public class GrayLetterRule {
 
 
     public static boolean needFindSupplier(Order order) {
-        if (!StringUtils.equalsAnyIgnoreCase(order.status, COMMON_GRAY_LETTER_REMARKS)) {
+        GrayLetterType type = GrayLetterType.getTypeFromStatus(order.status);
+        if (type == null) {
+            return false;
+        }
+
+        if (type.handleImmediately()) {
             return false;
         }
 
@@ -65,11 +72,9 @@ public class GrayLetterRule {
         }
 
         //对于第7-10天的灰条发灰条信
-        Date orderDate;
-        try {
-            orderDate = order.getPurchaseDate();
-        } catch (Exception e) {
-            orderDate = Dates.parseDateOfGoogleSheet(order.sheetName);
+        Date orderDate = getOrderDate(order);
+        if (orderDate == null) {
+            return false;
         }
 
         int days = Dates.daysBetween(orderDate, new Date());
@@ -80,44 +85,64 @@ public class GrayLetterRule {
     @SuppressWarnings("RedundantIfStatement")
     public static boolean needSendLetter(Order order, boolean waitToFindSupplier) {
 
-        if (!StringUtils.equalsAnyIgnoreCase(order.status, ArrayUtils.addAll(GRAY_LETTER_REMARKS, COMMON_GRAY_LETTER_REMARKS))) {
+        GrayLetterType type = GrayLetterType.getTypeFromStatus(order.status);
+        if (type == null) {
             return false;
         }
 
         //检查两天内的status（sg）和remark，对于remark含有 检查已经confirm后 随即发灰条信
         if (Strings.containsAnyIgnoreCase(order.remark, BLACKLIST_REMARKS)) {
+            LOGGER.info("order {} -  {} to send letter as remark contains blacklist {}", order.sheetName, order.order_id, order.remark);
             return true;
         }
 
         //c.检查两天内的status和remark，对于status和remark标有ng，wc，hp，ph，lw，检查已经confirm后随即发灰条信（wc除外）
-        if (StringUtils.equalsAnyIgnoreCase(order.status, GRAY_LETTER_REMARKS)) {
+        if (type.handleImmediately()) {
+            LOGGER.info("order {} -  {} to send letter as its status is {}", order.sheetName, order.order_id, order.status);
             return true;
         }
 
         if (!waitToFindSupplier) {
+            LOGGER.info("order {} -  {} to send letter as it's triggered manually, status is {}", order.sheetName, order.order_id, order.status);
             return true;
         }
 
         //对于第7-10天的灰条发灰条信
-        Date orderDate;
-        try {
-            orderDate = order.getPurchaseDate();
-        } catch (Exception e) {
-            orderDate = Dates.parseDateOfGoogleSheet(order.sheetName);
+        Date orderDate = getOrderDate(order);
+        if (orderDate == null) {
+            LOGGER.info("order {} -  {} to send letter as its purchase date {} is unknown", order.sheetName, order.order_id, order.purchase_date);
+            return true;
         }
 
         int days = Dates.daysBetween(orderDate, new Date());
         if (days >= 7 && days <= 10) {
+            LOGGER.info("order {} -  {} to send letter as its purchase date {} is {} days to today", order.sheetName, order.order_id, order.purchase_date, days);
             return true;
         }
 
         //d.对于us的快递单灰条（运费为6.99），重新找单失败后，检查已经confirm后随即发灰条信
         if (order.buyerExpeditedShipping()) {
+            LOGGER.info("order {} -  {} to send letter as it's expedited [{}] but no seller found", order.sheetName, order.order_id, order.shipping_service);
             return true;
         }
 
         return false;
     }
 
+    private static Date getOrderDate(Order order) {
+        Date orderDate = null;
+        try {
+            orderDate = order.getPurchaseDate();
+        } catch (Exception e) {
+            try {
+                orderDate = Dates.parseDateOfGoogleSheet(order.sheetName);
+            } catch (Exception e2) {
+                //
+                LOGGER.error("Fail to get order purchase date", order);
+            }
+        }
+
+        return orderDate;
+    }
 
 }
